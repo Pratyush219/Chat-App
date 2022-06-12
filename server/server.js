cff9ae6516d1abd3da64d242f0d0019d0dd05e9b
Pratyush219 <sspratyush7@gmail.com>
2022-06-13 00:01:15 +0530

    Initial client-server configuration

--- server/server.js
+++ server/server.js
@@ -1,23 +1,12 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketIO = require('socket.io');
-
-const publicPath = path.join(__dirname, '/../public');
-const port = process.env.PORT || 3000;
-
-let app = express();
-let server = http.createServer(app);
-let io = socketIO(server);
-
-app.use(express.static(publicPath));
-
-io.on('connection', (socket) => {
-    console.log('A new user just connected');
-    socket.on('disconnect', () => {
-        console.log('User was disconnected');
-    });
+const path = require('path')
+const express = require('express')
+const app = express()
+const socketIO = require('socket.io')
+const http = require('http')
+const exp = require('constants')
+let publicPath = path.join(__dirname, '/../public')
+app.use(express.static(publicPath))
+let port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Server connected to port ${port}`);
 })
-server.listen(port, () => {
-    console.log(`Server is up at port ${port}`);
-});
